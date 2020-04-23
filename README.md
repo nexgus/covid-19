450dc9b12dd3f84103ced7f921ccdea66dce4071
Augustus Chen <augustuschen@nexaiot.com>
2020-04-23 11:21:26 +0800

    Use bokeh instead of plotly

--- README.md
+++ README.md
@@ -4,15 +4,21 @@ API: [NovelCOVID API](https://github.com/NovelCOVID/API)
 API Document: https://documenter.getpostman.com/view/8854915/SzS7R6uu?version=latest  
 
 ## Install Dependencies
+### Linux
+```python
+pip install -r requirements.txt
+```
+### Windows
+**to-do: need extra process?**
 ```python
 pip install -r requirements.txt
 ```
 
 ## Syntax
 ```
-usage: covid-19.py [-h]
-                   [--key {cases,deaths,recovered,active,casesPerMillion,deathsPerMillion}]
-                   [--log]
+usage: choropleth.py [-h]
+                     [--key {cases,deaths,recovered,active,casesPerMillion,deathsPerMillion}]
+                     [--log] [--high HIGH] [--geodir GEODIR] [--res {10,50}]
 
 NCOVID-19 Breakout.
 
@@ -20,5 +26,9 @@ optional arguments:
   -h, --help            show this help message and exit
   --key {cases,deaths,recovered,active,casesPerMillion,deathsPerMillion}
                         Use total to select color. (default: active)
-  --log                 Log color. (default: False)
+  --log                 Log color scale. (default: False)
+  --high HIGH           High clamp value for color transform, in percentile.
+                        (default: 0.95)
+  --geodir GEODIR       Directory of geometry databases. (default: ./geo_data)
+  --res {10,50}         Resolution (meter) of map. (default: 50)
 ```
--- choropleth.py
+++ choropleth.py
@@ -1,33 +1,48 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
-import argparse
-#import datetime
-#import json
-import numpy as np
+"""Choropleth of COVID-19 Status
+Ref: https://towardsdatascience.com/a-complete-guide-to-an-interactive-geographical-map-using-python-f4c5197e23e0
+World map shape files: https://www.naturalearthdata.com/downloads/
+"""
+
+import geopandas as gpd
 import os
 import pandas as pd
-import plotly.graph_objects as go
-#import requests
 
 from api import NovelCOVIDApi
+from bokeh.models import ColorBar
+from bokeh.models import GeoJSONDataSource
+from bokeh.models import HoverTool
+from bokeh.models import LinearColorMapper
+from bokeh.models import LogColorMapper
+from bokeh.plotting import figure
+from bokeh.plotting import show
+from colorcet import CET_L18 as palette
 
 ##############################################################################################################
 KEYS = ['cases', 'deaths', 'recovered', 'active', 'casesPerMillion', 'deathsPerMillion']
 
 ##############################################################################################################
 def main(args):
+    # Read world geometry data
+    filepath = os.path.join(args.geodir, f'ne_{args.res}m_admin_0_countries.shp')
+    world = gpd.read_file(filepath)[['ADMIN', 'ADM0_A3', 'geometry']]
+    world.columns = ['country', 'country_code', 'geometry'] # Rename columns
+    # to-do: Do we need to remove Antarctica?
+    # antarctica = world.index[world['country']=='Antarctica'][0]
+    # world = world.drop(world.index[antarctica])
+
+    # Collect world summary for graph title.
     data = NovelCOVIDApi.get_all()
     updated = NovelCOVIDApi.to_datetime(data['updated']).astimezone().isoformat()
-    title = (f'COVID-19 Breakout ({args.key}) {updated}' +
-             f'<br>Cases: {data["cases"]}' + 
-             f'<br>Deaths: {data["deaths"]}' + 
-             f'<br>Recovered: {data["recovered"]}')
+    title = 'COVID-19 Breakout ({} {}) Cases: {}, Deaths: {}, Recovered: {}'.format(
+        updated, args.key, data['cases'], data['deaths'], data['recovered'])
 
+    # Collect data for each country.
     data = NovelCOVIDApi.get_countries()
     info = {
-        'country': [],
         'iso3': [],
-        'flag': [],
+        #'flag': [],
         'cases': [],
         'deaths': [],
         'recovered': [],
@@ -36,56 +51,95 @@ def main(args):
         'deathsPerMillion': [],
     }
     for country in data:
-        info['country'].append(country['country'])
         info['iso3'].append(country['countryInfo']['iso3'])
-        info['flag'].append(country['countryInfo']['flag'])
+        #info['flag'].append(country['countryInfo']['flag'])
         info['cases'].append(country['cases'])
         info['deaths'].append(country['deaths'])
         info['recovered'].append(country['recovered'])
         info['active'].append(country['active'])
         info['casesPerMillion'].append(country['casesPerOneMillion'])
         info['deathsPerMillion'].append(country['deathsPerOneMillion'])
-
     df = pd.DataFrame(info)
+    high = int(df.quantile(args.high).get(args.key))
+
+    # Merge datum into world geometry database
+    world = world.merge(df, left_on='country_code', right_on='iso3', how='left')
+    world.fillna('No data', inplace=True)
+    del df, info
+
+    geo = GeoJSONDataSource(geojson=world.to_json())
+    del world
+
     if args.log:
-        df['log'] = np.ma.log(df[args.key].to_numpy().astype(float))
-        args.key = 'log'
+        low = 100
+        ColorMapper = LogColorMapper
+    else:
+        low = 0
+        ColorMapper = LinearColorMapper
+    color_mapper = ColorMapper(palette=palette, low=low, high=high, nan_color='#d9d9d9')
 
-    df['hover_text'] = (df['country'] + 
-                        '<br>Cases:     ' + df['cases'].astype(str) + 
-                        '<br>Deaths:    ' + df['deaths'].astype(str) +
-                        '<br>Recovered: ' + df['recovered'].astype(str) +
-                        '<br>Active:    ' + df['active'].astype(str) +
-                        '<br>Cases/Million:  ' + df['casesPerMillion'].astype(str) +
-                        '<br>Deaths/Million: ' + df['deathsPerMillion'].astype(str))
+    tick_labels = {
+        f'{low}': f'{low}',
+        f'{high}': f'> {high}',
+    }
 
-    fig = go.Figure(
-        data=go.Choropleth(
-            locations=df['iso3'],
-            z=df[args.key],
-            colorscale = 'Reds',
-            showscale=False,
-            text=df['hover_text'],
-        ),
-        layout=go.Layout(
-            title=go.layout.Title(
-                text=title
-            ),
-            geo=go.layout.Geo(
-                resolution=50,
-                showcountries=True,
-                showlakes=False,
-            ),
-        ),
+    # The aspect ratio of Mercator projection is 1.65.
+    # https://en.wikipedia.org/wiki/Mercator_projection
+    fig_height = 800
+    fig_width = int(fig_height * 1.65)
+
+    color_bar = ColorBar(
+        color_mapper=color_mapper, 
+        label_standoff=8, 
+        width=fig_width, 
+        height=20,
+        border_line_color=None,
+        location=(0, 0), 
+        orientation='horizontal',
+        major_label_overrides=tick_labels,
     )
-    fig.show()
+
+    fig = figure(
+        title=title,
+        plot_width=fig_width, 
+        plot_height=fig_height, 
+        toolbar_location='left',
+    )
+    fig.xgrid.grid_line_color = None
+    fig.ygrid.grid_line_color = None
+    fig.patches(
+        'xs', 'ys', 
+        source=geo,
+        fill_color={
+            'field': args.key, 
+            'transform': color_mapper,
+        },
+        color='black',
+        line_width=0.3, 
+    )
+    fig.add_tools(
+        HoverTool(tooltips=[
+            ('Country', '@country'),
+            ('Cases', '@cases'),
+            ('Deaths', '@deaths'),
+            ('Recovered', '@recovered'),
+            ('Active', '@active'),
+            ('Cases/Million', '@casesPerMillion'),
+            ('Deaths/Million', '@deathsPerMillion'),
+    ]))
+    fig.add_layout(color_bar, 'below')
+    show(fig)
 
 ##############################################################################################################
 if __name__ == '__main__':
+    import argparse
     parser = argparse.ArgumentParser(description='NCOVID-19 Breakout.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--key',   help='Use total to select color.', choices=KEYS, default='active')
-    parser.add_argument('--log',   help='Log color.', action='store_true')
+    parser.add_argument('--key',  help='Use total to select color.', choices=KEYS, default='active')
+    parser.add_argument('--log',  help='Log color scale.', action='store_true')
+    parser.add_argument('--high', help='High clamp value for color transform, in percentile.', type=float, default=0.95)
+    parser.add_argument('--geodir', help='Directory of geometry databases.', default='./geo_data')
+    parser.add_argument('--res',  help='Resolution (meter) of map.', choices=[10, 50], default=50)
     args = parser.parse_args()
 
     main(args)
